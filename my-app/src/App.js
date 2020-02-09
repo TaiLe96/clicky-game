35de74adb699836c4fcc9a22d756bec3dcb9919d
Tai Le <tle449@gmail.com>
2020-02-08 20:07:52 -0800

    write a function for clickMe

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,37 +1,70 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import Card from "./components/Card";
+import images from "./images.json"
 
-function App() {
-  return(
-    <div className="App">
-      <Header/>
-      <Navbar/>
-      <Card/>
-    </div>
-  )
-}
+// function App() {
+//   return(
+//     <div className="App">
+//       <Header/>
+//       <Navbar/>
+//       <Card/>
+//     </div>
+//   )
+// }
 
 class App extends Component {
   state = {
-    image,
+    images,
     score: 0,
     highScore: 0,
     clicked: [],
     text: ""
   }
 
-chooseOne = id => {
+clickMe = id => {
   if (this.state.clicked.includes(id)){
-    this.setState({text: "Ehh, you pick the picture twice!!"})
+    this.setState({text: "Ehh, you pick the picture twice."})
     if(this.state.highScore < this.state.score){
-      
-    }
+      this.setState({highScore: this.state.score})
+    };
+    this.setState({clicked: []})
+    this.setState({score: 0})
   }
+  else{
+    this.setState({text: "Here we go!"})
+    this.setState({score: this.state.score + 1})
+    this.setSteate({click: [this.state.clicked, id]})
+  }
+  
 }
+
+render() {
+  return (
+          <div className="App">
+            <Header/>
+            <Navbar/>
+            {this.state.images.map(image =>(
+              <Card
+            id = {image.id}
+            key= {image.id}
+            image= {image.image}
+            clickMe= {this.clickMe}
+            />
+            ))}
+            
+          </div>
+    
+
+  )
+}
+
+
+
+
 }
 
 export default App;
--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -5,6 +5,7 @@ function Navbar(props){
         <div>
             <nav className="navbar fixed-top">
                 <h3>Score: {props.score} | Top Score: {props.highScore}</h3>
+                <h3>{props.text}</h3>
             </nav>
         </div>
     )
