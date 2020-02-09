fbf5f034adfe6fec6d99348fb274ad9ebec61c27
Tai Le <tle449@gmail.com>
2020-02-08 20:25:54 -0800

    added wrapper and styling for components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import Card from "./components/Card";
+import Wrapper from "./components/Wrapper";
 import images from "./images.json"
 
 // function App() {
@@ -25,42 +26,51 @@ class App extends Component {
     text: ""
   }
 
-clickMe = id => {
-  if (this.state.clicked.includes(id)){
-    this.setState({text: "Ehh, you pick the picture twice."})
-    if(this.state.highScore < this.state.score){
-      this.setState({highScore: this.state.score})
-    };
-    this.setState({clicked: []})
-    this.setState({score: 0})
-  }
-  else{
-    this.setState({text: "Here we go!"})
-    this.setState({score: this.state.score + 1})
-    this.setSteate({click: [this.state.clicked, id]})
-  }
-  
-}
+  clickMe = id => {
+    if (this.state.clicked.includes(id)) {
+      this.setState({ text: "Ehh, you pick the picture twice." })
+      if (this.state.highScore < this.state.score) {
+        this.setState({ highScore: this.state.score })
+      };
+      this.setState({ clicked: [] })
+      this.setState({ score: 0 })
+    }
+    else {
+      this.setState({ text: "Here we go!" })
+      this.setState({ score: this.state.score + 1 })
+      this.setSteate({ click: [this.state.clicked, id] })
+    }
 
-render() {
-  return (
-          <div className="App">
-            <Header/>
-            <Navbar/>
-            {this.state.images.map(image =>(
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar
+          text={this.state.text}
+          score={this.state.score}
+          highScore={this.state.highScore} />
+        <div className="jumbotron text-center">
+          <h1>Bachelor Clicky Game</h1>
+          <p>Click on image for 1 point but be careful and don't click on one more than once!</p>
+          <Wrapper>
+            {this.state.images.map(image => (
               <Card
-            id = {image.id}
-            key= {image.id}
-            image= {image.image}
-            clickMe= {this.clickMe}
-            />
+                id={image.id}
+                key={image.id}
+                image={image.image}
+                clickMe={this.clickMe}
+              />
             ))}
-            
-          </div>
-    
+          </Wrapper>
+        </div>
+      </div>
 
-  )
-}
+
+
+
+    )
+  }
 
 
 
--- my-app/src/components/Card.js
+++ my-app/src/components/Card.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "../styles/card.css"
 
 function Card(props){
     return(
         <div className="row">
-            <div>
-                <div>
+            <div className="col-md-1">
+                <div className="card">
                     <img src={props.image} alt={props.id} onClick={() => props.clickMe(props.id)}/>
                 </div>
             </div>
