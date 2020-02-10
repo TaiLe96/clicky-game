bb6f0f75a11bbff4d1be0285611a49d3eb159eef
Tai Le <tle449@gmail.com>
2020-02-10 11:56:13 -0800

    Updates

---
+++ precache-manifest.ad6372dd884e9b055a822dabb7fa7e84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b77f9d667ff295b8b08ea4088728042e",
+    "url": "/clicky-game/./index.html"
+  },
+  {
+    "revision": "b4bc8f49478e0c46fab8",
+    "url": "/clicky-game/./static/css/main.15b28ca8.chunk.css"
+  },
+  {
+    "revision": "2b28977e3b6df7765ab2",
+    "url": "/clicky-game/./static/js/2.f0a6a870.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/clicky-game/./static/js/2.f0a6a870.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4bc8f49478e0c46fab8",
+    "url": "/clicky-game/./static/js/main.91c96935.chunk.js"
+  },
+  {
+    "revision": "f510beb13259278517f8",
+    "url": "/clicky-game/./static/js/runtime-main.8df45e42.js"
+  }
+]);
